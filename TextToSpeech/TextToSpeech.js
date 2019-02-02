8d5ff9b0b8e7270a7c24f393d4ea4aa3fddb187e
Deeksha Shankhdher <shankhda@mail.uc.edu>
2019-02-02 16:34:58 -0500

    Final changes

--- TextToSpeech/TextToSpeech.js
+++ TextToSpeech/TextToSpeech.js
@@ -5,18 +5,22 @@ inputText=document.getElementById('input-text');
 voiceSelect=document.getElementById('voice-select');
 playbackRate=document.getElementById('playback-rate');
 playbackRateValue=document.getElementById('playback-rate-value');
-pitch=document.getElementById('pitch-rate');
+pitch=document.getElementById('pitch');
 pitchValue=document.getElementById('pitch-value');
+body = document.querySelector('body');
 
 voices =[];
 
 function populateVoices() {
     voices= speech.getVoices();
-    voices.forEach(voice => { 
+    voices.forEach(function(voice) { 
 
         option = document.createElement('option')
         option.textContent = voice.name + '('+voice.lang +')';
         
+        option.setAttribute('voice-name',voice.name);
+        option.setAttribute('voice-lang',voice.name);
+
         voiceSelect.appendChild(option);
     });
 };
@@ -26,3 +30,57 @@ populateVoices();
 if (speech.onvoiceschanged !==undefined){
     speech.onvoiceschanged = populateVoices;
 }
+
+function speak() {
+   
+    if (speech.speaking) {
+      alert('Already speaking...');
+      return;
+    }
+    if (inputText.value !== '') {
+      
+      body.style.background = '#141414 url(img/wave.gif)';
+      body.style.backgroundRepeat = 'repeat-x';
+      body.style.backgroundSize = '100% 100%';
+  
+     
+      speechText = new SpeechSynthesisUtterance(inputText.value);
+  
+     
+      speechText.onend = function(event) {  
+        body.style.background = '#141414';
+      };
+  
+      speechText.onerror = function(event) {
+        alert('Something went wrong');
+      };
+  
+     
+      selectedVoice = voiceSelect.selectedOptions[0].getAttribute('voice-name');
+  
+      voices.forEach(function(voice) {
+        if (voice.name === selectedVoice) {
+            speechText.voice = voice;
+        }
+      });
+  
+      
+      speechText.rate = playbackRate.value;
+      speechText.pitch = pitch.value;
+     
+      speech.speak(speechText);
+    }
+  };
+  
+  
+mainForm.addEventListener('submit', function(submit) {
+    submit.preventDefault();
+    speak();
+    inputText.blur();
+});
+  
+playbackRate.addEventListener('change', function(){playbackRateValue.textContent = playbackRate.value});
+
+pitch.addEventListener('change',function(){pitchValue.textContent = pitch.value});
+
+voiceSelect.addEventListener('change', speak);
